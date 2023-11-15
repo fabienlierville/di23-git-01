42af6a037dc3f2e3d23d9c754616aa27755e3e96
Seynax <seyronax@gmail.com>
2023-11-15 14:54:26 +0100

    html, css and js fix

--- groupeA/script.js
+++ groupeA/script.js
@@ -1,24 +1,24 @@
-let p2 = document.getElementById("p2");
-
+let p2 = document.getElementById("p1");
 p2.addEventListener('mouseover', (e) => {
     console.log("Bonjour");
 });
 
-
-let p1 = document.getElementById("pl");
-
+let p1 = document.getElementById("p2");
 pl.addEventListener('mouseover', (e) => {
     console.log("bonjour");
 })
-let p3 = document.getElementById("p2");
 
+let p3 = document.getElementById("p3");
 p3.addEventListener('mouseover', (e) => {
     console.log("Bonjour");
 });
 
-
-let p4 = document.getElementById("pl");
-
+let p4 = document.getElementById("p4");
 p4.addEventListener('mouseover', (e) => {
+    console.log("bonjour");
+})
+
+let p5 = document.getElementById("p5");
+p5.addEventListener('mouseover', (e) => {
     console.log("bonjour");
 })
