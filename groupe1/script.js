1918536a6927468dfe4675611ca8c53703780f77
Seynax <seyronax@gmail.com>
2023-11-15 15:27:17 +0100

    js fix

--- groupe1/script.js
+++ groupe1/script.js
@@ -1,24 +1,43 @@
-let p2 = document.getElementById("p1");
-p2.addEventListener('mouseover', (e) => {
-    console.log("Bonjour");
-});
+window.onload = function ()
+{
+    const p1 = document.getElementById("p1");
+    if(p1 != null)
+    {
+        console.log("A");
+        p1.addEventListener("mouseover", function (e) {
+            console.log("Bonjour " + p1.innerText);
+        });
+    }
 
-let p1 = document.getElementById("p2");
-pl.addEventListener('mouseover', (e) => {
-    console.log("bonjour");
-})
+    let p2 = document.getElementById('p2');
+    if(p2 != null)
+    {
+        p2.addEventListener('mouseover', function (e) {
+            console.log("Bonjour " + p2.innerText);
+        });
+    }
 
-let p3 = document.getElementById("p3");
-p3.addEventListener('mouseover', (e) => {
-    console.log("Bonjour");
-});
+    let p3 = document.getElementById('p3');
+    if(p3 != null)
+    {
+        p3.addEventListener('mouseover', function (e) {
+            console.log("Bonjour " + p3.innerText);
+        });
+    }
 
-let p4 = document.getElementById("p4");
-p4.addEventListener('mouseover', (e) => {
-    console.log("bonjour");
-})
+    let p4 = document.getElementById('p4');
+    if(p4 != null)
+    {
+        p4.addEventListener('mouseover', function (e) {
+            console.log("Bonjour " + p4.innerText);
+        });
+    }
 
-let p5 = document.getElementById("p5");
-p5.addEventListener('mouseover', (e) => {
-    console.log("bonjour");
-})
+    let p5 = document.getElementById('p5');
+    if(p5 != null)
+    {
+        p5.addEventListener('mouseover', function (e) {
+            console.log("Bonjour " + p5.innerText);
+        });
+    }
+}
