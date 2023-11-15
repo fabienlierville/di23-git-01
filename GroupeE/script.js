374f879c802c08d77710511cdb6ec1d957f197ee
ACLAIN <amaury.clain@viacesi.fr>
2023-11-15 13:42:59 +0100

    js

--- GroupeE/script.js
+++ GroupeE/script.js
@@ -1,18 +1,26 @@
+
+// P1
 let p1 = document.getElementById('p1');
 
 p1.addEventListener('mouseover', (e) => {
     console.log("P1");
 })
+
+//P2
 let p2 = document.getElementById('p2');
 
 p2.addEventListener('mouseover', (e) => {
     console.log("P2");
 })
+
+//P3
 let p3 = document.getElementById('p1');
 
 p3.addEventListener('mouseover', (e) => {
     console.log("P3");
 })
+
+// P4
 let p4 = document.getElementById('p4');
 
 p4.addEventListener('mouseover', (e) => {
