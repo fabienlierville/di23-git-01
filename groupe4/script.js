840fc9024499da191ad816b92f6e8cb760ecc878
JeanneGrenet <jeanne.grenet@gmail.com>
2023-11-15 13:57:24 +0100

    bonjour name

--- groupe4/script.js
+++ groupe4/script.js
@@ -1,23 +1,23 @@
 let p1 = document.getElementById("p1");
-let name = p1.textContent;
+let name1 = p1.textContent;
 p1.addEventListener('mouseover', (e)=>(
-    console.log("Bonjour "+name)
+    console.log("Bonjour "+name1)
 ))
 
 let p2 = document.getElementById("p2");
-name = p2.textContent;
+let name2 = p2.textContent;
 p2.addEventListener('mouseover', (e)=>(
-    console.log("Bonjour " + name)
+    console.log("Bonjour " + name2)
 ))
 
 let p3 = document.getElementById("p3");
-name = p3.textContent;
+let name3 = p3.textContent;
 p3.addEventListener('mouseover', (e)=>(
-    console.log("Bonjour" + name)
+    console.log("Bonjour" + name3)
 ))
 
 let p4 = document.getElementById("p4");
-name = p4.textContent;
+let name4 = p4.textContent;
 p4.addEventListener('mouseover', (e)=>(
-    console.log("Bonjour " + name)
+    console.log("Bonjour " + name4)
 ))
