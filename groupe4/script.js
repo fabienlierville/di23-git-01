2eb4bbe08c9cffc8a1bf3c2889e70014700c51f6
JeanneGrenet <jeanne.grenet@gmail.com>
2023-11-15 14:27:08 +0100

    vhjkbjml

--- groupe4/script.js
+++ groupe4/script.js
@@ -1,28 +1,24 @@
 let p1 = document.getElementById("p1");
 let name1 = p1.textContent;
 p1.addEventListener('mouseover', (e)=>(
-    console.log("Bonjour")
     console.log("Bonjour "+name1)
 ))
 
 let p2 = document.getElementById("p2");
 let name2 = p2.textContent;
 p2.addEventListener('mouseover', (e)=>(
-    console.log("Bonjour")
     console.log("Bonjour " + name2)
 ))
 
 let p3 = document.getElementById("p3");
 let name3 = p3.textContent;
 p3.addEventListener('mouseover', (e)=>(
-    console.log("Bonjour")
     console.log("Bonjour " + name3)
 ))
 
 let p4 = document.getElementById("p4");
 let name4 = p4.textContent;
 p4.addEventListener('mouseover', (e)=>(
-    console.log("Bonjour")
     console.log("Bonjour " + name4)
 
 ))
