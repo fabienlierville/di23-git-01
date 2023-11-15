924deadcd9f40249bc7ed5f4acce2acccfddcf01
Arthur <arthur.reynet@viacesi.fr>
2023-11-15 13:58:55 +0100

    erreur gestionnaire d'évènement

--- Groupe c/script.js
+++ Groupe c/script.js
@@ -1,23 +1,23 @@
 let p1 = document.getElementById("p1");
 
-pl.addEventListener('mouseover', (e) => {
+p1.addEventListener('mouseover', (e) => {
     console.log("Bonjour"+p1.innerText);
 });
 
 let p2 = document.getElementById("p2");
 
-pl.addEventListener('mouseover', (e) => {
+p2.addEventListener('mouseover', (e) => {
     console.log("Bonjour")+p2.innerText;
 });
 
 let p3 = document.getElementById("p3");
 
-pl.addEventListener('mouseover', (e) => {
+p3.addEventListener('mouseover', (e) => {
     console.log("Bonjour"+p3.innerText);
 });
 
 let p4 = document.getElementById("p4");
 
-pl.addEventListener('mouseover', (e) => {
+p4.addEventListener('mouseover', (e) => {
     console.log("Bonjour"+p4.innerText);
 });
