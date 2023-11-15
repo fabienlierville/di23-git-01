87d4eb8fbf94821ef4daad3d63d94456f655311a
Arthur <arthur.reynet@viacesi.fr>
2023-11-15 14:00:24 +0100

    IL FAUT SAUVER ALI

--- Groupe c/script.js
+++ Groupe c/script.js
@@ -1,23 +1,23 @@
 let p1 = document.getElementById("p1");
 
 p1.addEventListener('mouseover', (e) => {
-    console.log("Bonjour"+p1.innerText);
+    console.log("Bonjour"+ p1.innerText);
 });
 
 let p2 = document.getElementById("p2");
 
 p2.addEventListener('mouseover', (e) => {
-    console.log("Bonjour")+p2.innerText;
+    console.log("Bonjour")+ p2.innerText;
 });
 
 let p3 = document.getElementById("p3");
 
 p3.addEventListener('mouseover', (e) => {
-    console.log("Bonjour"+p3.innerText);
+    console.log("Bonjour"+ p3.innerText);
 });
 
 let p4 = document.getElementById("p4");
 
 p4.addEventListener('mouseover', (e) => {
-    console.log("Bonjour"+p4.innerText);
+    console.log("Bonjour"+ p4.innerText);
 });
